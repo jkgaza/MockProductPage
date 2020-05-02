33826657856fb0888e04e2fc3c590fc63d8435f9
Jordan Gaza <Gaza>
2020-05-02 13:18:29 -0400

    Doesn't like querySelectorAll for some reason

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var likeBtn = document.querySelector('.ico');
+let likeBtn = document.querySelector('.ico');
 
 likeBtn.addEventListener('click', function() {
      likeBtn.classList.toggle('liked');
 });
 
-document.addEventListener('keydown', function(key){
-   if(key.key === 'l' || key.key === 'L') {
-      likeBtn.classList.toggle('liked');
-   }
-});
+// document.addEventListener('keydown', function(key){
+//    if(key.key === 'l' || key.key === 'L') {
+//       likeBtn.classList.toggle('liked');
+//    }
+// });
